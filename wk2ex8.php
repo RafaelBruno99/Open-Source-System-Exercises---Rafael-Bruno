4ed3301c1c6c04f43bfed89d4cc97fd12d102ec0
RafaelBruno99 <55978113+RafaelBruno99@users.noreply.github.com>
2020-02-10 10:40:02 +0000

    Incresed the count from 5 to 7

--- wk2ex8.php
+++ wk2ex8.php
@@ -9,7 +9,7 @@
     $topModules[5] = "Web Services";
     $topModules[6] = "Mobile Systems";
 
-    for ($count=0; $count < 5 ; $count++) {
+    for ($count=0; $count < 7 ; $count++) {
       echo "$count module is $topModules[$count]. </br>";
     }
   ?>
