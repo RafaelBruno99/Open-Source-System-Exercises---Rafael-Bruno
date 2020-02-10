533667b7b22e0bf6d79f5264a2d564802bb0cd09
RafaelBruno99 <55978113+RafaelBruno99@users.noreply.github.com>
2020-02-10 09:48:57 +0000

    Create wk2ex1.php
    Week 2 Exercise 2

---
+++ wk2ex1.php
@@ -0,0 +1,17 @@
+<html>
+<body>
+  <?php
+    $hourlyrate = 5.75;
+    $hoursperweek = 40;
+    $gross = $hourlyrate * $hoursperweek;
+  ?>
+
+  <head></head>
+
+  <p> My gross wage is <?php print("$gross"); ?>
+
+
+
+
+</body>
+</html>
