ab5d79adb2a43f869b97efc03f7f7afa184673c2
RafaelBruno99 <55978113+RafaelBruno99@users.noreply.github.com>
2020-02-10 10:38:26 +0000

    Exercise 8 Week 2

---
+++ wk2ex8.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+  <?php
+    $topModules[0] = "Internet System Development";
+    $topModules[1] = "Programming 1";
+    $topModules[2] = "Programming 2";
+    $topModules[3] = "OOAD";
+    $topModules[4] = "Software Engineering";
+
+    for ($count=0; $count < 5 ; $count++) {
+      echo "$count module is $topModules[$count]. </br>";
+    }
+  ?>
+
+</body>
+</html>
