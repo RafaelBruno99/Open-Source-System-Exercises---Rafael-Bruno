30a40d5fafc2571e25df284a87f90eb010eaaea1
RafaelBruno99 <55978113+RafaelBruno99@users.noreply.github.com>
2020-02-10 10:22:47 +0000

    Exercise 6, I have assign values to all the elements due to an error

---
+++ wk2ex6.php
@@ -0,0 +1,22 @@
+<html>
+<body>
+  <?php
+    $marks[0] = 35;
+    $marks[1] = 40;
+    $marks[2] = 50;
+    $marks[3] = 60;
+    $marks[4] = 70;
+    $marks[5] = 80;
+
+
+    echo "Index 0 = $marks[0]. </br>";
+    echo "Index 1 = $marks[1]. </br>";
+    echo "Index 2 = $marks[2]. </br>";
+    echo "Index 3 = $marks[3]. </br>";
+    echo "Index 4 = $marks[4]. </br>";
+    echo "Index 5 = $marks[5]. </br>";
+
+  ?>
+
+</body>
+</html>
