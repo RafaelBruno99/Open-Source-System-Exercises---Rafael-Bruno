67074e791f2f172a5ea53fb2035a337870721a92
RafaelBruno99 <55978113+RafaelBruno99@users.noreply.github.com>
2020-02-10 10:03:30 +0000

    Small Changes in term of spacing

--- wk2ex3.php
+++ wk2ex3.php
@@ -2,12 +2,12 @@
 <body>
   <?php
     define("PASS_GRADE", 40);
-    echo "You need" . PASS_GRADE . "% or more to pass. </br>";
+    echo "You need " . PASS_GRADE . "% or more to pass. </br>";
     $grade = PASS_GRADE + 15;
     echo "To achive a merit you need $grade % or more </br>";
     $grade = PASS_GRADE + 30;
     echo "To achieve a distinction you need $grade % or more </br>";
-    
+
   ?>
 
 </body>
