6af37c7cbb3338566b628eeb310cccf8c0db2dd5
RafaelBruno99 <55978113+RafaelBruno99@users.noreply.github.com>
2020-02-10 10:39:41 +0000

    Added two more modules

--- wk2ex8.php
+++ wk2ex8.php
@@ -6,6 +6,8 @@
     $topModules[2] = "Programming 2";
     $topModules[3] = "OOAD";
     $topModules[4] = "Software Engineering";
+    $topModules[5] = "Web Services";
+    $topModules[6] = "Mobile Systems";
 
     for ($count=0; $count < 5 ; $count++) {
       echo "$count module is $topModules[$count]. </br>";
