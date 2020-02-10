5f0024f6eb80e165bf0c33a16877e074c1de7d9e
RafaelBruno99 <55978113+RafaelBruno99@users.noreply.github.com>
2020-02-10 09:56:41 +0000

    Exercise 2 Week 2

---
+++ wk2ex2.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+  <?php
+    $myage = "None of your business </br>";
+    echo $myage;
+    $myage = 21;
+    echo ($myage . "</br>");
+    $myage = 21.75;
+    echo $myage;
+
+  ?>
+
+</body>
+</html>
